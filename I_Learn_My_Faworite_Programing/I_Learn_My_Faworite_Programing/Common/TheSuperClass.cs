ffee18b2dc8da594ed845b4cc3af5ce81c528a17
a1980dominov <a1980dominov@gmail.com>
2024-01-20 22:38:31 +0200

    The end

---
+++ I_Learn_My_Faworite_Programing/I_Learn_My_Faworite_Programing/Common/TheSuperClass.cs
@@ -0,0 +1,27 @@
+﻿using I_Learn_My_Faworite_Programing.PersonWith;
+using I_Learn_My_Faworite_Programing.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I_Learn_My_Faworite_Programing.Common
+{
+    class TheSuperClass
+    {
+        public void MySuperMethod()
+        {
+            Developer developer = new Developer("Fred");
+            developer.MySuperProgress();
+            developer.StartedProgrammer(new ProgrammingAnBasic("Basic", "1995"));
+            developer.MySuperProgress();
+            developer.StartedProgrammer(new ProgrammingAnConsole("Console APP", "2019"));
+            developer.MySuperProgress();
+            developer.StartedProgrammer(new ProgramingWPF("C#,WPF", "2020"));
+            developer.MySuperProgress();
+            developer.StartedProgrammer(new ProgrammingASPNET("ASPnet.Core", "2021"));
+            developer.MySuperProgress();
+            developer.StartedProgrammer(new ProgrammingAnBeckEnd("React,Angular,Pyton", "2022-2023"));
+            developer.MySuperProgress();
+        }
+    }
+}
--- I_Learn_My_Faworite_Programing/I_Learn_My_Faworite_Programing/Program.cs
+++ I_Learn_My_Faworite_Programing/I_Learn_My_Faworite_Programing/Program.cs
@@ -1,4 +1,5 @@
-﻿using I_Learn_My_Faworite_Programing.PersonWith;
+﻿using I_Learn_My_Faworite_Programing.Common;
+using I_Learn_My_Faworite_Programing.PersonWith;
 using I_Learn_My_Faworite_Programing.Strategies;
 using System;
 
@@ -8,18 +9,8 @@ namespace I_Learn_My_Faworite_Programing
     {
         static void Main(string[] args)
         {
-            Developer developer = new Developer("Fred");
-            developer.MySuperProgress();
-            developer.StartedProgrammer(new ProgrammingAnBasic("Basic", "1995"));
-            developer.MySuperProgress();
-            developer.StartedProgrammer(new ProgrammingAnConsole("Console APP", "2019"));
-            developer.MySuperProgress();
-            developer.StartedProgrammer(new ProgramingWPF("C#,WPF", "2020"));
-            developer.MySuperProgress();
-            developer.StartedProgrammer(new ProgrammingASPNET("ASPnet.Core", "2022"));
-            developer.MySuperProgress();
-            developer.StartedProgrammer(new ProgrammingAnBeckEnd("React,Angular,Pyton", "2023"));
-            developer.MySuperProgress();
+            TheSuperClass theSuperClass = new TheSuperClass();
+            theSuperClass.MySuperMethod();
         }
     }
 }
